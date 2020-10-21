b24ca132ef3518082d81df3ed04f5f7fdd5b089a
CsharpSyntraWestKortrijk <61596529+CSharpSyntraWest@users.noreply.github.com>
2020-10-21 11:38:56 +0200

    Add files via upload

---
+++ LES 5/Oefeningen-INNER-LEFT-RIGHT JOINS.sql
@@ -0,0 +1,12 @@
+USE [BierenDb];
+--INNER JOIN tussen 2 tabellen
+--1. Geef alle informatie uit de tabellen bieren met soorten, gebruik een Inner join op kolommen SoortNr
+--2. Geef alle informatie uit de tabellen bieren met brouwers, gebruik een inner join op kolommen BrouwerNr
+--3. INNER JOIN tussen 3 tabellen
+--Geef alle informatie uit de tabellen brouwers, bieren en soorten, gebruik inner joins op kolommen soortnr en brouwernr
+
+--RIGHT JOIN tussen 2 tabellen
+--4. Geef alle informatie uit bieren en brouwers, geef ook de informatie van de bieren die geen brouwer hebben
+--LEFT JOIN tussen 2 tabellen
+--5. Geef alle informatie uit bieren en soorten, geef ook de informatie van de bieren die geen soort hebben
+
