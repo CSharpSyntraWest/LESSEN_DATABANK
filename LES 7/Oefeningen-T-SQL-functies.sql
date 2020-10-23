8f05cdf37be35009914be4aa80a1c75e0e4f3e6f
CsharpSyntraWestKortrijk <61596529+CSharpSyntraWest@users.noreply.github.com>
2020-10-23 08:57:39 +0200

    Add files via upload

---
+++ LES 7/Oefeningen-T-SQL-functies.sql
@@ -0,0 +1,33 @@
+USE BierenDB;
+
+--1. Geef de biernamen en de positie weer van character a in de namen van alle bieren
+--(eerste letter in string begint op positie 1 in T-Sql !!!)
+
+--2. Geef de biernamen en de 2e,3e en 4e karakters (gebruik substring) 
+--uit de namen van alle bieren
+
+--3. Geef de biernamen en hun lengte (aantal karakters)
+
+--4. Geef de biernamen die exact 5 karakters lang zijn, 
+--alfabetisch gesorteerd
+
+--5. Geef per lengte van alle biernamen, het aantal bieren met 
+-- de naam van die lengte, omgekeerd gesorteerd op lengte van biernamen
+
+--6. Geef alle biernamen in hoofdletters en in kleine letters
+
+
+--7. Verwijder via sql de buitenste spaties van tekst '   HELLO WORLD   ' 
+
+
+--8. Verwijder de voorste spaties uit tekst '   HELLO WORLD   ' 
+
+
+--9. Verwijder de spaties achteraan uit tekst '   HELLO WORLD   ' 
+
+
+--10. Geef de eerste 3 karakters uit alle biernamen
+
+
+--11. Geef de laatste 3 karakters uit alle biernamen en verwijder alle 
+-- spaties voor en achteraan
